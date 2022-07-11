e3565392d72c70d813292b57ce387a6fa9806b1d
daniel <mr.daniels.2025@gmail.com>
2022-07-11 17:38:56 -0600

    intial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+}
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type:  type
+  }
+  return menuItem
 
 }
 
+function addIngredients(items, ingredients) {
+
+if(ingredients.includes(items)){
+  ingredients.pop();
+}
+  ingredients[ingredients.length] = items;
+  console.log(ingredients);
+  return ingredients;
+}
+
+function formatPrice(price) {
+
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+   return price * 0.9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+   title: title,
+   ingredients: ingredients,
+   type: menuItemType,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
